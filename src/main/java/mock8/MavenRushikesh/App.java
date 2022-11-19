0b6cdae3d30fd38d22b3677893dea6843c73cc76
Roshan <Roshan@lenovo>
2022-11-19 08:53:37 +0530

    hi i am mahesh

---
+++ src/main/java/mock8/MavenRushikesh/App.java
@@ -0,0 +1,13 @@
+package mock8.MavenRushikesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
